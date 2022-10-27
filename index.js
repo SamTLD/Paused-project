8802ed5bbe6dd1f6a19df5495d1da1809f60b902
SamTLD <111151431+SamTLD@users.noreply.github.com>
2022-10-27 10:38:43 +0530

    Paused-project

---
+++ index.js
@@ -0,0 +1,29 @@
+/*const circle = {
+    radius : 1,
+    location : {
+        x : 1,
+        y : 1
+    },
+    draw : function() {
+        console.log('draw');
+    }
+};
+circle.draw();
+
+const circle = {
+    radius : 1,
+    location : {
+        x : 1,
+        y : 1
+    },
+};*/
+
+const circle = {
+    radius : 2,
+    location : {
+        x : 2,
+        y : 2
+    },
+};
+
+circle.draw();
